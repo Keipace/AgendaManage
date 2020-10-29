01c83901082e2a2c88a9d6aa5a51480ef19ac131
Keipace <virtualEmailForKeipace@virtual.com>
2020-10-29 20:28:31 +0800

    填充目标清单到选项卡

--- app/src/main/java/com/privateproject/agendamanage/MainActivity.java
+++ app/src/main/java/com/privateproject/agendamanage/MainActivity.java
@@ -1,26 +1,19 @@
 package com.privateproject.agendamanage;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.privateproject.agendamanage.databinding.ActivityMainBinding;
 import com.privateproject.agendamanage.fragment.FragmentTest;
 import com.privateproject.agendamanage.fragment.GoalListFragment;
 
-
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding pageXml;
     private GoalListFragment goalListFragment;
     private FragmentTest fragmentTest;
-
-    private Button qingdanButton;
-    private Button richengButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,56 +23,47 @@ public class MainActivity extends AppCompatActivity {
         pageXml = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(pageXml.getRoot());
 
-
-        selectTab(0);//默认选中第一个Tab
-        this.goalListFragment = new GoalListFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.mianActivity_container_contrainlayout, this.goalListFragment, "goalList").commitAllowingStateLoss();
+        selectTab(0);//默认选中第一个Tab，即清单
     }
 
-
-
     public void onClick(View v) {
-        //先将四个ImageButton置为灰色
-//        resetImgs();
         switch (v.getId()) {
-            case R.id.mianActivity_richeng_btn:
-                selectTab(0);//当点击的是微信的Tab就选中微信的Tab
-                break;
             case R.id.mianActivity_qingdan_btn:
+                selectTab(0);
+                break;
+            case R.id.mianActivity_richeng_btn:
                 selectTab(1);
                 break;
+            case R.id.mianActivity_tongji_btn:
+                //统计页面
+                break;
+            case R.id.mianActivity_me_btn:
+                //我 页面
+                break;
         }
-
     }
 
     //进行选中Tab的处理
     private void selectTab(int i) {
-        //获取FragmentManager对象
-        FragmentManager manager = getSupportFragmentManager();
         //获取FragmentTransaction对象
-        FragmentTransaction transaction = manager.beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         //先隐藏所有的Fragment
         hideFragments(transaction);
         switch (i) {
-            //当选中点击的是微信的Tab时
             case 0:
-//                qingdanButton.setText("beidianl");
-                //如果微信对应的Fragment没有实例化，则进行实例化，并显示出来
-                if (fragmentTest == null) {
-                    fragmentTest = new FragmentTest();
-                    transaction.add(R.id.mianActivity_container_contrainlayout, fragmentTest);
+                if (goalListFragment == null) {
+                    goalListFragment = new GoalListFragment();
+                    transaction.add(R.id.mianActivity_container_contrainlayout, goalListFragment).commitAllowingStateLoss();
                 } else {
-                    //如果微信对应的Fragment已经实例化，则直接显示出来
-                    transaction.show(fragmentTest);
+                    transaction.show(goalListFragment).commitAllowingStateLoss();
                 }
                 break;
             case 1:
-//                richengButton.setText("wobeidianl");
-                if (goalListFragment == null) {
-                    goalListFragment = new GoalListFragment();
-                    transaction.add(R.id.mianActivity_container_contrainlayout, goalListFragment);
+                if (fragmentTest == null) {
+                    fragmentTest = new FragmentTest();
+                    transaction.add(R.id.mianActivity_container_contrainlayout, fragmentTest).commitAllowingStateLoss();
                 } else {
-                    transaction.show(goalListFragment);
+                    transaction.show(fragmentTest).commitAllowingStateLoss();
                 }
                 break;
         }
